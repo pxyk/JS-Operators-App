14f0cbcdd56aeae6c89e5dbb7e9d2b14cde8cfc9
Mert Aytemiz <mertaytemizwork@gmail.com>
2023-06-01 09:26:02 +0300

    Second Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+    //3 sayıdan en büyüğünü bulan uygulama
+    var a= prompt("sayı giriniz:");
+    var b= prompt("sayı giriniz:");
+    var c= prompt("sayı giriniz:");
+
+    console.log("a sayısı en büyük mü ? :",(a>b) && (a>c));
+    console.log("b sayısı en büyük mü ? :",(b>a) && (b>c));
+    console.log("c sayısı en büyük mü ? :",(c>a) && (c>b)); //true
